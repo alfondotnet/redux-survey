37d2e8bd0852746073cfc2289dedd86cf3cb362a
Alfonso Perez <alfon@ratslap.com>
2015-10-12 16:41:28 +0200

    test button with dummy answer

--- test/components/QuestionList_spec.jsx
+++ test/components/QuestionList_spec.jsx
@@ -5,7 +5,9 @@ import {expect} from 'chai';
 const {
         renderIntoDocument,
         findAllInRenderedTree,
-        isDOMComponent
+        isDOMComponent,
+        Simulate,
+        scryRenderedDOMComponentsWithTag
       } = React.addons.TestUtils;
 
 function scryRenderedDOMComponentsWithClassName(tree, className) {
@@ -14,21 +16,29 @@ function scryRenderedDOMComponentsWithClassName(tree, className) {
   });
 }
 
+
+// So we create a Question list of 2 test questions for testing
+const questionListData = [
+                    {
+                      question: 'Question 1',
+                      answer: null,
+                      possibleAnswers: [1,2,3],
+                      getScore: function(score) { return score; }
+                    },
+                    {
+                      question: 'Question 2',
+                      answer: null,
+                      possibleAnswers: [1,2,3],
+                      getScore: function(score) { return score + 1; }
+                    }
+                  ];
+
 describe('QuestionList', () => {
   it('renders a pair of questions', () => {
+
     const component = renderIntoDocument(
-      <QuestionList list={[{
-                            question: 'Question 1',
-                            score: null,
-                            possibleAnswers: [1,2,3],
-                            extraPoints: 1,
-                           },
-                           {
-                            question: 'Question 2',
-                            score: null,
-                            possibleAnswers: [1,2,3],
-                            extraPoints: 2,
-                           }]} />
+      <QuestionList
+        list={questionListData} />
     );
 
     const divItems = scryRenderedDOMComponentsWithClassName(component, 'questionTitle');
@@ -36,6 +46,25 @@ describe('QuestionList', () => {
     expect(divItems.length).to.equal(2);
     expect(divItems[0].textContent).to.equal('Question 1');
     expect(divItems[1].textContent).to.equal('Question 2');
+  });
+
+
+  it('invokes callback when a question is answered', () => {
+
+    let answered;
+    const callback = (index, answer) => answered = answer;
+
+    const component = renderIntoDocument(
+      <QuestionList
+        list={questionListData}
+        onAnswer={callback} />
+    );
+
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(answered).to.equal(3);
+
   });
 
 });
