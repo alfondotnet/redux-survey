295e8b3fef4f1e65f3c0d3b2c6b62c178a06fff3
Alfonso Perez <alfon@ratslap.com>
2015-10-12 12:33:06 +0200

    question list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import QuestionList from './QuestionList';
 
+const listQuestions = ['Question 1', 'Question 2'];
+
 export default React.createClass({
   render: function() {
     return <div className="app">
-      <QuestionList />
+      <QuestionList list={listQuestions} />
     </div>;
   }
 });
--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 export default React.createClass({
+  getList: function() {
+    return this.props.list || [];
+  },
   render: function() {
-    return <div>Question list</div>;
+    return <div>
+      <h2>Question list</h2>
+      <ul>
+      {this.getList().map(question =>
+        <li>{question}</li>
+      )}
+      </ul>
+    </div>;
   }
 });
