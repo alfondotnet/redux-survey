259bac51e39625f9e15c6b78715a91ee2e5664f8
Alfonso Perez <alfon@ratslap.com>
2015-12-27 18:46:28 +0100

    Root prod

---
+++ src/containers/Root.prod.jsx
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import { ReduxRouter } from 'redux-router';
+
+export default class Root extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <Provider store={store}>
+        <div>
+          <ReduxRouter
+            routes={[]}
+            location={{}}
+            params={{}}
+            components={[]} />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+}
